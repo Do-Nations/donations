682b127b52efa58c9133ce32f77a54899c43cd64
Osama Baker <edewanjy@hotmail.com>
2021-04-16 23:44:48 +0300

    Added the form function

--- js/volunteer.js
+++ js/volunteer.js
@@ -17,28 +17,58 @@ let ammanlist=document.getElementById('amman-list');
 let irbidlist=document.getElementById('irbid-list');
 let eventform= document.getElementById('volunteerform')
 
+let firstName;
+let lastName;
+let age;
+let phoneNumber;
+
 
 
 function showlist (event)
 {
-
+    
     event.preventDefault()
-
+    
     eventform.style.display = "none";
     if(zarqa.checked===true)
     {
         zarqalist.style.display = "block";
     }
 
-     if (amman.checked===true)
+    if (amman.checked===true)
     {
         ammanlist.style.display = "block";
-
+        
     }
-
-     if(irbid.checked===true)
+    
+    if(irbid.checked===true)
     {
 irbidlist.style.display = "block";
-    }
-      
 }
+      
+
+    
+firstName = document.getElementById("firstName").value
+    
+    localStorage.setItem('name',firstName);
+
+    lastName = document.getElementById("lastName").value
+    localStorage.setItem('lname',lastName);
+
+    age = document.getElementById("age").value
+    localStorage.setItem('age',age);
+
+    phoneNumber = document.getElementById("phoneNumber").value
+    localStorage.setItem('phoneNumber',phoneNumber);
+    
+    
+}
+let data =localStorage.getItem('name');
+let data2 =localStorage.getItem('lname');
+let data3 =localStorage.getItem('age');
+let data4 =localStorage.getItem('phoneNumber');
+
+document.getElementById("firstName").value =  data;
+document.getElementById("lastName").value =  data2;
+document.getElementById("age").value =  data3;
+document.getElementById("phoneNumber").value =  data4;
