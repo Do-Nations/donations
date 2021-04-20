50c4d2480f40eea03841d0642b00c48219316cd9
bahaamq <bahaa.mq@gmail.com>
2021-04-20 14:10:15 +0300

    jsUpdates and css animation

--- js/volunteer.js
+++ js/volunteer.js
@@ -13,7 +13,7 @@ const orders = document.getElementById('list');
 
 let parent=document.getElementById('list')
 
-let ulElement = document.createElement('ul')
+let divElement = document.createElement('div')
 
 
 
@@ -128,45 +128,42 @@ let imageUpload = document.getElementById('url');
 
 
 function render(){
-  parent.textContent='';
+  divElement.textContent='';
   
  getData(); 
- 
-  parent.appendChild(ulElement);
 
-  // const drinkLI = document.createElement('li');
-  // const infoP = document.createElement('p');
+ 
+  parent.appendChild(divElement);
+
+
 
   for(let i= 0 ; i < Members.member.length;i++)
 {
-  const drinkLI = document.createElement('li');
+  let paraghh = document.createElement('p');
 
-  // let image = document.getElementById('imgURL');
-  // image.src = imageUpload1;
-
-  const infoP = document.createElement('p');
-
-
-
-
-
-
-
-// console.log(imageUpload1)
 
   let imageUpload = document.createElement('img')
-  drinkLI.appendChild(imageUpload)
+  divElement.appendChild(imageUpload)
   imageUpload.src = Members.member[i].imageUpload;
   imageUpload.style.width = '156px';
 
   
-  infoP.textContent = `Your name is : ${Members.member[i].fname} ${Members.member[i].lname}, Age: ${Members.member[i].age}, Phone Number: ${Members.member[i].phone}, Gender: ${Members.member[i].type}`;
-  drinkLI.appendChild(infoP);
-  orders.appendChild(drinkLI);
+  paraghh.innerHTML = ` Name is : ${Members.member[i].fname} ${Members.member[i].lname},
+  <br> Age: ${Members.member[i].age}, <br>
+  Phone Number: ${Members.member[i].phone}, <br>
+   Gender: ${Members.member[i].type} <br>`;
+
+   divElement.appendChild(paraghh)
+
 
   // console.log('test members num'+ Members.member.length)
 
+
+  
 }
+
+let test =document.getElementById('firstName').required = true;
+
 
  }
 
@@ -183,12 +180,8 @@ function getData()
  Members.member = JSON.parse(data);  
 
  }
-
- 
-
  
 }
-
 
 
 
@@ -209,13 +202,6 @@ let ammanlist=document.getElementById('amman-list');
 let irbidlist=document.getElementById('irbid-list');
 let eventform= document.getElementById('volunteerform')
 
-let firstName;
-let lastName;
-let age;
-let phoneNumber;
-
-
-
 function showItem (event)
 {
     
@@ -224,47 +210,37 @@ function showItem (event)
   
     if(zarqa.checked===true)
     {
-        zarqalist.style.display = "block";
+        zarqalist.style.display = "inline-block";
+    }
+    else
+    {
+      zarqalist.style.display = "none";
+
     }
 
     if (amman.checked===true)
     {
-        ammanlist.style.display = "block";
+        ammanlist.style.display = "inline-block";
         
     }
+
+    else
+    {
+      ammanlist.style.display = "none";
+
+    }
+
+
     
     if(irbid.checked===true)
     {
-irbidlist.style.display = "block";
-}
-      
-
+irbidlist.style.display = "inline-block";
+    }
+    else
     
-firstName = document.getElementById("firstName").value
-    
-    localStorage.setItem('name',firstName);
-
-    localStorage.setItem('image', imageUpload);
-
-    lastName = document.getElementById("lastName").value
-    localStorage.setItem('lname',lastName);
-
-    age = document.getElementById("age").value
-    localStorage.setItem('age',age);
-
-    phoneNumber = document.getElementById("phoneNumber").value
-    localStorage.setItem('phoneNumber',phoneNumber);
-    
+    {
+  irbidlist.style.display = "none";
+      }
     
 }
-let data =localStorage.getItem('name');
-let data2 =localStorage.getItem('lname');
-let data3 =localStorage.getItem('age');
-let data4 =localStorage.getItem('phoneNumber');
-
-document.getElementById("firstName").value =  data;
-document.getElementById("lastName").value =  data2;
-document.getElementById("age").value =  data3;
-document.getElementById("phoneNumber").value =  data4;
-
 
