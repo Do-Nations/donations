e82cff4fdb9e3c3b2a7e863562da0641982a5a04
Ahmad-A92-3 <ahmad.alsyed92@gmail.com>
2021-04-20 11:05:23 +0300

    table-cart

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,7 @@
 
 let products = localStorage.getItem('Products');
 products = JSON.parse(products);
-console.log('This is my product list', products);
+// console.log('This is my product list', products);
 
 function megaSum() {
     let all = 0;
@@ -15,115 +15,126 @@ function megaSum() {
 
 function listProduct() {
 
-    let parent = document.getElementById('main');
+    let parent = document.getElementById('table');
     console.log(parent);
-    let productForm = document.createElement('form');
-    parent.appendChild(productForm);
-    // aLL  tOTAL ELEMENT
-    let all = document.getElementById('all');
-    let megaTotal = megaSum();
+    // parent.setAttribute('style','display:flex;');
+    // let productForm = document.createElement('form');
+    // parent.appendChild(productForm);
+    // // aLL  tOTAL ELEMENT
+    // let all = document.getElementById('all');
+    // let megaTotal = megaSum(); 
+    console.log(products)
 
-    for (let i = 0; i < products.length; i++) {
+    if(products===null){
+        console.log('the storage is empty');
+        let thElement=document.getElementById('main');
+        let tableId=document.getElementById('table')
+        console.log(thElement);
+        thElement.removeChild(tableId);
+    }else{    
+        for (let i = 0; i < products.length; i++) {
 
-        if (products[i].purshaceNo != 0) {
-            // set the initial value of t
-            products[i].total = products[i].price;
-            megaTotal = megaSum();
-            all.innerHTML = `Total: ${megaTotal}`;
-
-            let divisons = document.createElement('div');
-            divisons.setAttribute('class', 'box');
-            productForm.appendChild(divisons);
-            // Create image elemnt holds all attributes
-            let productImage = document.createElement('img');
-            divisons.appendChild(productImage);
-            productImage.setAttribute('src', products[i].url);
-            // create h4 element that includes the name of product
-            let productDescription = document.createElement('h4');
-            productDescription.textContent = products[i].name;
-            divisons.appendChild(productDescription);
-            // For show the item unit price 
-            let unitprice = document.createElement('h4');
-            // productLable.setAttribute('for',products[i].name);       
-            divisons.appendChild(unitprice);
-            unitprice.textContent = 'Unit Price:' + products[i].price + ' JOD';
-            // create input element to enter number of items 
-            // Its label 
-            let itemNoLabel = document.createElement('label');
-            itemNoLabel.setAttribute('for', products[i].name);
-            divisons.appendChild(itemNoLabel);
-            itemNoLabel.textContent = 'Quantity';
-            // input
-            let itemNo = document.createElement('input');
-            itemNo.setAttribute('type', 'number');
-            itemNo.setAttribute('name', products[i].name);
-            itemNo.setAttribute('value', '1')
-            // itemNo.setAttribute('change',total(this.value));
-            // itemNo.setAttribute('value','number');
-            divisons.appendChild(itemNo);
-
-            // add event of change 
-            itemNo.addEventListener("change", total);
-
-            // Add Total for each product 
-            let totalPriceLabel = document.createElement('label');
-            totalPriceLabel.setAttribute('for', products[i].name);
-            divisons.appendChild(totalPriceLabel);
-            totalPriceLabel.textContent = 'Total:' + products[i].price
-
-            function total(event) {
-                // return itemNo.value*products[i].price;
-                let value = itemNo.value * products[i].price;
-                console.log(value);
-                totalPriceLabel.textContent = 'Total:' + value + 'JOD';
-                products[i].total = value;
-                products[i].purshaceNo = itemNo.value;
-                console.log('The total quantity', products[i].purshaceNo);
+            if (products[i].purshaceNo != 0) {            
+                // parent.setAttribute('style','display:block');
+                // set the initial value of t
+                products[i].total = products[i].price;
                 megaTotal = megaSum();
                 all.innerHTML = `Total: ${megaTotal}`;
+                            
+                // Create the table row
+                let row = document.createElement('tr');
+                parent.appendChild(row);
+                // creat the first cell of the row-name 
+                detailName=document.createElement('td');
+                row.appendChild(detailName);
+                detailName.textContent=products[i].name;
+                // crerate the 2nd detail-photos
+                detailPhoto=document.createElement('td');
+                row.appendChild(detailPhoto);            
+                let productImage = document.createElement('img');
+                detailPhoto.appendChild(productImage);
+                productImage.setAttribute('src', products[i].url);
+                // cretae the 3rd detail-unit price
+                detailPrice=document.createElement('td');
+                row.appendChild(detailPrice);
+                detailPrice.textContent=products[i].price+ ' JOD';
+                // create the 4th detail-number of items
+                let itemNo= document.createElement('input');
+                row.appendChild(itemNo);
+                itemNo.setAttribute('type', 'number');
+                itemNo.setAttribute('name', products[i].name);
+                itemNo.setAttribute('value', '1');
+                // create the 5th detail-total price including the change event. 
+                detailtotal=document.createElement('td');
+                row.appendChild(detailtotal);
+                detailtotal.setAttribute('id',products[i].name)
+                detailtotal.textContent=products[i].price+ ' JOD';
+
+                
+                
+            
+                // // itemNo.setAttribute('change',total(this.value));
+                
+
+                // add event of change 
+                itemNo.addEventListener("change", total);
+
+                // Add Total for each product 
+                // let totalPriceLabel = document.createElement('label');
+                // totalPriceLabel.setAttribute('for', products[i].name);
+                // divisons.appendChild(totalPriceLabel);
+                // totalPriceLabel.textContent = 'Total:' + products[i].price
+
+                function total(event) {
+                    console.log(event.target);
+                    let changedIndex=products.indexOf(event.target.name);
+                    let ourtarget=document.getElementById(event.target.name);
+                    let value = itemNo.value * products[i].price;
+                    console.log(value);
+                    ourtarget.textContent =  value + ' JOD';
+                    products[i].total = value;
+                    products[i].purshaceNo = itemNo.value;
+                    console.log('The total quantity', products[i].purshaceNo);
+                    megaTotal = megaSum();
+                    all.innerHTML = `Total: ${megaTotal}`;
+                }
             }
-
         }
+    }  
 
+   
 
-
-    }
-
-    let divi = document.createElement('div');
-    divi.setAttribute('class', 'subm');
-    productForm.appendChild(divi);
-
-    let linkCart = document.createElement('a');
-    linkCart.setAttribute('href', '../html/cart.html');
-    divi.appendChild(linkCart);
-
+   
+    // crete the checkout button
     let sbmitted = document.createElement('input');
-    sbmitted.setAttribute('type', 'submit');
+    sbmitted.setAttribute('type', 'button');
     sbmitted.setAttribute('name', 'submit');
     sbmitted.setAttribute('value', 'Checkout');
-    linkCart.appendChild(sbmitted);
+    parent.appendChild(sbmitted);
 
+    // event related to submitt buttom
 
-    // productForm.addEventListener('submit',updatePurshase);
+    sbmitted.addEventListener('click',checkout);
 
-    // function updatePurshase(event){
-    //     localStorage.clear();
-    //     event.preventDefault();
-    //     // targ=JSON.stringify(event.target);
-    //     console.log('This is the event',event);    
+    function checkout(event){
+        localStorage.clear();
+        event.preventDefault();
+        alert('Thanks for your donation\n your total purchase value is '+ megaTotal+' JoD');
+        let thElement=document.getElementById('th');
+        console.log(thElement);
+        thElement.textContent='';
 
-    //     for (let i=0;i<products.length;i++){
-    //         let TorF =event.target[i].checked;
-    //         if (TorF==true){
-    //             products[i].purshaceNo++;
-    //             console.log(products[i].purshaceNo);             
-    //         }
-    //     }
-    //     let stringProduct=JSON.stringify(products)
-    //     localStorage.setItem('Products',stringProduct);
+        // console.log('This is the event',event);    
 
-
-    // }
-
+        // for (let i=0;i<products.length;i++){
+        //     let TorF =event.target[i].checked;
+        //     if (TorF==true){
+        //         products[i].purshaceNo++;
+        //         console.log(products[i].purshaceNo);             
+        //     }
+        // }
+        // let stringProduct=JSON.stringify(products)
+        // localStorage.setItem('Products',stringProduct);
+    }
 }
 listProduct();
